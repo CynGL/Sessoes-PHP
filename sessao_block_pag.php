673d219d5c6e415049a27f6a0d69244003dd6e99
Cyntia Lino <100882616+CynGL@users.noreply.github.com>
2023-05-29 20:15:13 -0300

    Add files via upload

---
+++ sessao_block_pag.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Bloqueando páginas por sessões</title>
+</head>
+<body>
+<form name = "Logar" method="post" action="sessoes_block.php">
+	Nome: <br>
+	<input type="text" name="nome"> <br>
+	Senha: <br>
+	<input type="password" name="senha"> <br>
+	<br>
+	<br>
+	<input type="submit" name="enviar" value="Enviar">
+
+
+
+</form>
+
+
+
+</body>
+</html>
